46af67b6c3adc0d086965e927cb591107dc61594
Drew Green <agreenbhm@gmail.com>
2026-06-04 13:28:48 -0400

    Fully-functional

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
 FROM ubuntu:24.04
 
-RUN apt-get update && apt-get upgrade -yf && apt-get install -yf glusterfs-server && \
-    cp -a /etc/glusterfs /etc/glusterfs.defaults
+RUN apt-get update && apt-get upgrade -yf && apt-get install -yf glusterfs-server
+
+RUN cp -a /etc/glusterfs /etc/glusterfs.defaults
+RUN cp -a /var/lib/glusterd /var/lib/glusterd.defaults
 
 COPY docker-entrypoint.sh /usr/local/bin/docker-entrypoint.sh
 RUN chmod +x /usr/local/bin/docker-entrypoint.sh
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,10 +7,19 @@ if [ -z "$(ls -A /etc/glusterfs 2>/dev/null)" ]; then
     cp -a /etc/glusterfs.defaults/. /etc/glusterfs/
 fi
 
+if [ -z "$(ls -A /var/lib/glusterd 2>/dev/null)" ]; then
+    echo "Seeding /var/lib/glusterd with default configs..."
+    chmod 755 /var/lib/glusterd
+    cp -a /var/lib/glusterd.defaults/. /var/lib/glusterd/
+fi
+
 sed -i "s/option max-port.*$/option max-port $GLUSTER_MAX_PORT/g" /etc/glusterfs/glusterd.vol
 
-exec /usr/sbin/glusterd \
-    --pid-file /var/run/glusterd.pid \
-    --no-daemon \
-    --log-file /dev/stdout \
-    "$@"
+if [ "$ENV" = "DEV" ]; then
+    echo "Starting in development mode..."
+    /etc/init.d/ssh start
+fi
+
+/usr/sbin/glusterd --pid-file /var/run/glusterd.pid --log-file /dev/stdout --log-level INFO
+
+exec "$@"
